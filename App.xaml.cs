518136fc92fd96a4a9d4c4d0a56a34c6bd17dd2b
Gerry <gerrym751@gmail.com>
2025-07-31 11:12:15 +0200

    Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Actom_Works_Orders
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
